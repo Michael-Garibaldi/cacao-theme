18f008881a93c5d6a16f0f6828d375e3796adc59
Michael-Garibaldi <michael.garibaldi1@gmail.com>
2025-11-21 16:50:42 -0500

    Add files via upload
    One change made - STILL WIP  use at your own risk!!

--- wip/cacao-dark-theme.el
+++ wip/cacao-dark-theme.el
@@ -37,42 +37,19 @@
                  :slant normal :weight medium :height 112  :width normal
                  :foundry "outline" :family "Times New Roman Bold 20"))))
 
- `(fixed-pitch ((t (:height 100 :family "DejaVu Sans Mono Bold" :foreground "#fb8b54" :weight bold))))
+ `(fixed-pitch ((t (:height 100 :family "DejaVu Sans Mono Bold" :foreground "#fb8b54" :weight bold)))))
 
 
 ;;frame-tab
-`(frame-tabs-buffer-tab ((t (:background "#e2af89" :foreground "#2e1604" :weight bold ))))
-`(frame-tabs-highlight-tab ((t (:background "#fadebe" :foreground "#edb677"))))
-`(frame-tab-selected-tab ((t (:box (:line-width 2 :color "#fadebe" (:background "#a16827" :foreground "#f28305"))))))
-
-
-
-;; Basic settings
-`(cursor ((t (:background "#fadebe" :family "Times New Roman Bold 20"))))
-`(header-line ((t (:background "#fadebe" :foreground "#804809" :box (:line-width -1 :color "#f2d7b8" :style released-button) :height 0.9 :family "Times New Roman Bold 20"))))
-`(highlight ((t (:background "fadebe" :foreground "#8a4a01" :family "Times New Roman Bold 20"))))
-`(fixed-pitch ((t (:background "#fadebe" :foreground "#e5a763" :family "Times New Roman Bold 20"))))
-`(minibuffer-prompt ((t (:foreground "#fb8b54" :weight bold))))
-`(mode-line ((t (:background "#e5a763" :foreground "#5c3307" :box 3 :height 1.0  :weight bold :family "Times New Roman Book 20"))))
-`(mode-line-buffer-id ((t (:background "#e5a763" :foreground "#5c3307" :weight bold :height 0.9 :family "Times New Roman Book 20"))))
-`(mode-line-emphasis ((t (:weight bold :family "Times New Roman Book  35"))))
-`(mode-line-highlight ((t (:box (:line-width 2 :color "#180d08" :style released-button) :family "Times new Roman Book 20 Bold"))))
-
-;; mode-line inactive is a bit different, a box was added and the background color has been changed to be a bit different than the foreground.
-
-`(mode-line-inactive ((t (:background "#70370b" :foreground "#a35b0f" :box (:line-width 3 :color "#f0db86" :style released-button) :overline nil :underline nil :height 105 :weight bold :family "Times New Roman Book 20  Bold"))))
-
-`(tab-bar ((t (:background "#693c28" :foreground "f2e4c1"))))
-`(tooltip ((t (:background "#f9f0dd" :foreground "460a04" :family "Times New Roman Bold 20"))))
-`(isearch ((t (:background "#70370b" :foreground "a35b0f" :weight bold))))
-`(isearch-fail ((t (:background "#e5a763" :foreground "#5c3307" :weight bold))))
-
-
-
+`(frame-tabs-buffer ((t (:background "#180d08" :foreground "#FD9723"))))
+`(frame-tabs-highlight-tab ((t (:background"#f57c00"  :foreground "#e180d08"))))
 ;;;###autoload
 (when load-file-name
   (add-to-list 'custom-theme-load-path
-	       (file-name-as-directory (file-name-directory load-file-name)))))
+	       (file-name-as-directory (file-name-directory load-file-name))))
+
+
+;; modeline
 
 (provide-theme 'cacao-dark)
 
