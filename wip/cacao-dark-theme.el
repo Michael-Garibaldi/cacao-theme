6dc12296eda1e84b28c80efb3717445710c2fa01
Michael-Garibaldi <michael.garibaldi1@gmail.com>
2025-12-10 16:54:31 -0500

    Add files via upload
    WORK in Progress - USE AT YOUR OWN RISK

--- wip/cacao-dark-theme.el
+++ wip/cacao-dark-theme.el
@@ -21,19 +21,15 @@
 (deftheme cacao-dark
   "This  theme is based on an accidentally inversed image")
 
-;; Custom variables to make changing colors easier.
-;;(let ((class '((class color)))
-;;      (colorbg1 "#310a02")
-;;      (colorbg2 "#e65a4")
-;;      (colorfg1 "#e5a763")
-;;      (colorfg2 "#e6dac2") ;; Used in highligt face.
-;;      )
 
 (custom-theme-set-faces
  'cacao-dark
 
+;;currently using #320d00 as the background. Will try #411610 or #5e150c as well
+
+ 
  ;; default setting
- `(default ((t ( :background "#180d08" :foreground  "#e5a763"
+ `(default ((t ( :background "#3a120a" :foreground  "#e5a763"
                  :slant normal :weight medium :height 112  :width normal
                  :foundry "outline" :family "Times New Roman Bold 20"))))
 
@@ -42,16 +38,17 @@
 
 ;; Basic Settings
 `(cursor ((t (:background "#ff8845" :family "Times New Roman Bold 20"))))
-`(header-line ((t (background "#4a1e1a" :foreground "#d17f2d" :box (:line-width -1 :color "#ddb194" :style release-button) :height 0.9 :family "Times New Roman Bold 20")))) ; literally the header line above package-install.
+`(header-line ((t (:background "#4a1e1a" :foreground "#d17f2d" :box (:line-width -1 :color "#ddb194" :style release-button) :height 0.9 :family "Times New Roman Bold 20")))) 
 `(highight ((t (:background "#ddb194"  :foreground "#847973" :family "Times New Roman BOld 20"))))
 `(isearch ((t (:background "#f5b49c" :foreground "#170a06" :weight bold))))
 `(isearch-fail ((t ( :foreground "#f22200" :weight bold))))
 `(minibuffer-prompt ((t (:foreground  "#da4c00" :weight bold))))
-`(mode-line ((nil (:background "#2c0a09" :foreground "#bb3103" :box 2 :height 1.0 :weight bold :family "Times New Roman Book 20"))))
+`(mode-line ((t (:background "#2c0a09" :foreground "#bb3103" :box 2 :height 1.0 :weight bold :family "Times New Roman Book 20"))))
+`(mode-line-active ((t (:background "#2c0a09" :foreground "bb3103" :box 2 :height 1.0  :weight bold :family "Times New Roman Book 20"))))
 `(mode-line-emphasis ((t (:weight bold :family "Times New Roman Book 35"))))
-`(mode-line-highlight ((t (:box (:line-width 2 :color "#ff8845" :style released-button):family "Times New Roman Book 20"))))
-`(mode-line-inactive ((t ((:background "#eba897" :background "#eb6938" :box (:line-width 8 :color "#eb6938") :family "Times New Roman Book 20 bold")))))
-`(tab-bar ((t (:backgrond "#ff8845" :foreground "d171f2d"))))
+`(mode-line-highlight ((t (:box (:line-width 2 :color "#ff8845" :style released-button):family "Times New Roman Book 20 Bold"))))
+`(mode-line-inactive ((t ((:background "#eba897" :foreground "#eb6938" :box (:line-width 8 :color "#eb6938") :family "Times New Roman Book 20 bold")))))
+`(tab-bar ((t (:background "#ff8845" :foreground "d171f2d"))))
 `(tooltip ((t (:background "#f5b49c" :foreground "#76351c" :family "Times New Roman Bold 20"))))
 
 
@@ -64,13 +61,45 @@
 `(font-lock-doc-face ((t ("#c2461c" :weight bold :family "Times New Roman 20"))))
 `(font-lock-function-name-face ((t (:foreground "#a82c03"))))
 `(font-lock-keyword-face ((t (:foreground "#bb3103"))))
+`(font-lock-preprocessor-face ((t (:foreground "#eb9444"))))
+`(font-lock-regexp-grouping-backslash ((t (:foreground "#974024"))))
+`(font-lock-regexp-grouping-construct ((t (:foreground "#c18d6d"))))
+`(font-lock-string-face ((t (:foreground "#c85f1e"))))
+`(font-lock-type-face ((t (:foreground "##c35f14"))))
+`(button ((t (:background "#180508" :foreground "#d17f2d" :weight bold ))))
+`(lazy-highight ((t (:background "#c35f14" :foreground "#482315"))))
+`(link ((t (:foreground "#7eb944" :weight bold))))
+`(link-visited ((t (:foreground "#b74342"))))
+`(match ((t (:foreground "#cf9a74"))))
+`(next-error ((t (:foreground "#9d4223"))))
+`(query-replace ((t (:background "e7ddc9":foreground "#7f5e42" :weight bold))))
 
 
+;; eshell 
 
-;;frame-tab
-`(frame-tabs-buffer-tab ((t (:box (:line-width 2 :color "#ff8845" (:background "#180d08" :foreground "#d17f2d"))))))
+`(ehsell-ls-archive ((t (:foreground "#e9bb63"))))
+`(eshell-ls-backup ((t (:foreground "#f1be6c"))))
+`(eshell-ls-clutter ((t (:foreground "#956638"))))
+`(eshell-ls-directory ((t (:foreground "#f2bc6d" :weight bold))))
+`(eshell-ls-executable ((t (:foreground "#79bc38" :weight bold))))
+`(eshell-ls-missing ((t (:foreground "#e69d9c" :weight bold))))
+`(eshell-ls-product ((t (:foreground "#d8ac67" :weight bold))))
+`(eshell-ls-prompt ((t (:foreground "#e9c264" :weight bold))))
+`(ehsell-ls-readonly ((t (:foreground "#962a2b" :weight bold))))
+`(eshell-ls-special ((t (:foreground "#91ab66" :weight bold))))
+`(eshell-ls-symlink ((t (:foreground "#9b7f4b" :weight bold))))
+`(eshell-ls-unreadable ((t (:foreground "#c32731"))))
+
+;;frame-tabs
+
+;;experimential code to combat frame-tabs :inherit tabs
+
+
+;;`(frame-tabs ((nil (:box (:line-width 2 :color "#ff8845") (:background "#180d08" :foreground "#d17f2d" )))))
+`(frame-tabs-buffer-tab ((t (:background "#180508" :foreground "#d17f2d" ))))
 `(frame-tabs-highlight-tab ((t (:background "#fa7911" :foreground "#ff8845"))))
-`(frame-tabs-selected-tab ((t (:background "#180d08" :foreground "#ea9e3e"))))
+`(frame-tabs-selected-tab ((t (:box (:line-width 2  :color "#ff8845" (:background "#180d08" :foreground "#ea9e3e"))))))
+
 
 ;;`(frame-tabs-selected-tab ((t (:background "#180d08" :foreground "#fd9723"))))
 
@@ -80,10 +109,8 @@
 	       (file-name-as-directory (file-name-directory load-file-name))))
 
 
-;; modeline
 
 (provide-theme 'cacao-dark)
-
 ;; Local Variables
 ;; End
 ;;; cacao-theme.el ends here
